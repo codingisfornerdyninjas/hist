20f0c02775c0162580a6582989a4e4ae07b77568
codingisfornerdyninjas <128403544+codingisfornerdyninjas@users.noreply.github.com>
2023-11-17 15:19:41 -0600

    Update flooder.js

--- flooder.js
+++ flooder.js
@@ -1,1 +1,7 @@
+  function rand(numb) {
+for(let i = 0; i < numb; i++){
+  x = window.open("https://codingisfornerdyninjas.github.io/hist/?q= "+ Math.floor(Math.random() * 500000))
+  setTimeout(function(){x.close()} , 10)
+}
 
+} 
